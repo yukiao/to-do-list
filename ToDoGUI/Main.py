91fbf8f2e87f20b837c83e2d9b8cbb7be46ceda2
yukiao <lembang19sony@gmail.com>
2020-05-28 13:40:53 +0800

    Adding GUI for ToDoList App

---
+++ ToDoGUI/Main.py
@@ -0,0 +1,54 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import *
+import sys
+import Login as login
+
+class Home(QWidget):
+    def __init__(self):
+        super(Home,self).__init__()
+        self.setGeometry(200,200,400,200)
+        self.initUi()
+    
+    def initUi(self):
+        self.usernameLabel = QLabel('Username')
+        self.usernameField = QLineEdit()
+        self.passwordLabel = QLabel('Password')
+        self.passwordField = QLineEdit()
+        self.passwordField.setEchoMode(QtWidgets.QLineEdit.Password)
+
+        self.button = QPushButton("Login")
+        self.button.clicked.connect(self.onClicked)
+
+        self.grid = QGridLayout()
+
+        self.grid.addWidget(self.usernameLabel,0,0)
+        self.grid.addWidget(self.usernameField,0,1)
+        self.grid.addWidget(self.passwordLabel,1,0)
+        self.grid.addWidget(self.passwordField,1,1)
+        self.grid.addWidget(self.button,2,2)
+        self.grid.setHorizontalSpacing(20)
+        
+        self.setLayout(self.grid)
+
+    def onClicked(self):
+        self.username = self.usernameField.text()
+        self.password = self.passwordField.text()
+        
+        if login.Login().authentication(self.username,self.password):
+            pass
+        else:
+            msg = QMessageBox()
+            msg.setWindowTitle("Error")
+            msg.setText('Wrong username/password')
+            msg.setIcon(QMessageBox.Critical)
+
+            x = msg.exec_()
+
+def window():
+    app = QApplication(sys.argv)
+    win = Home()
+    win.setWindowTitle("TodoList")
+    win.show()
+    sys.exit(app.exec_())
+
+window()
